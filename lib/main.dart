51c7c2cb584163c681632f0c598f4e722d0336f6
Minh truong <minh.truong@s3corp.com.vn>
2021-01-14 18:10:23 +0700

    Add first Ui

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_music/pages/home_page/home_page.dart';
+import 'package:flutter_music/pages/playing_song/playing_song.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme: GoogleFonts.mcLarenTextTheme(Theme.of(context).textTheme)),
+      home: HomePage(),
+      routes: {'/song': (context) => PlayingSong()},
+    );
+  }
+}
